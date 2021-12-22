f27ee09f55767392d602371b28600c8241e44ac2
F G <f@g.com>
2021-12-22 20:21:08 +0300

    Fix code for CI success

--- echo_bot.py
+++ echo_bot.py
@@ -1,10 +1,9 @@
-import os
 import telebot
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot("TOKEN")
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
