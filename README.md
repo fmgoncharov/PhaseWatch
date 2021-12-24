7d4bf10a13d288dc1cf046b6426af258d69572d8
F G <f@g.com>
2021-12-24 14:23:32 +0300

    fix push to Docker Hub

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PhaseWatch
 
 [![GitHub Super-Linter](https://github.com/fmgoncharov/PhaseWatch/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
+
+**Автор проекта**: Гончаров Фёдор Михайлович, студент БПМИ202
+
+**Бот**: @PhaseWatch
+
+**Описание проекта**: Бот принимаем размеры картинки и выдаёт изображение с соответсвующими размерами
+
+**Сервер**: от Amazon AWC
+
+**Автоматический запуск**: через WatchTower
--- echo_bot.py
+++ echo_bot.py
@@ -1,16 +1,21 @@
 import telebot
+import os
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot(os.environ["BOT_TOKEN"])
 
 
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    return bot.reply_to(message, "Напиши мне размеры картинки через пробел. \n Например, 400 400")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    try:
+        sizes = message.text.split()
+        return bot.send_photo(message.chat.id, 'https://picsum.photos/' + sizes[0] + '/' + sizes[1])
+    except:
+        return bot.reply_to(message, "Неправильный формат. Попробуй ещё раз")
 
 
 bot.infinity_polling()
