74ae24323d21d9ad74cd0bb697666e00b8bdfc51
F G <f@g.com>
2021-12-25 21:35:09 +0300

    fix echo_bot.py check that WatchTower will use changes

--- echo_bot.py
+++ echo_bot.py
@@ -8,7 +8,9 @@ bot = telebot.TeleBot(os.environ["BOT_TOKEN"])
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     return bot.reply_to(
-        message, "Напишите мне размеры картинки через пробел. \nНапример, 2000 2000"
+        message, "Напишите мне размеры картинки через пробел\n"
+                 "Числа должны быть натуральными, не больше 5000\n"
+                 "Например, 2000 2000"
     )
 
 
@@ -18,12 +20,13 @@ def echo_all(message):
         sizes = message.text.split()
         if len(sizes) != 2:
             raise IndexError
-        if not (sizes[0].isdecimal() and sizes[1].isdecimal()):
-            raise SyntaxError
+        int(sizes[0].isdecimal())
+        int(sizes[1].isdecimal())
         if not (0 < int(sizes[0]) <= 5000 and 0 < int(sizes[1]) <= 5000):
             raise ValueError
+        address = "https://picsum.photos/" + sizes[0] + "/" + sizes[1]
         return bot.send_photo(
-            message.chat.id, "https://picsum.photos/" + sizes[0] + "/" + sizes[1]
+            message.chat.id, address
         )
     except IndexError:
         return bot.reply_to(message, "Неправильный формат. Введите ДВА числа")
@@ -31,7 +34,7 @@ def echo_all(message):
         return bot.reply_to(message, "Неправильный формат. Введите два ЧИСЛА")
     except ValueError:
         return bot.reply_to(
-            message, "Неправильный формат. Введите два числа из отрезка [1; 5000]"
+            message, "Неправильный формат. Введите два натуральных числа из отрезка [1; 5000]"
         )
 
 
